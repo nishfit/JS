4fb7c9a251412a9ed269191f8585e64d7bf875a9
Nishant Singh <143175203+nishfit@users.noreply.github.com>
2023-10-17 19:57:49 +0000

    done conversion

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+
+
+
+const accountId = 13220
+let acctEmail = "nish.nsingh@gmail.com"
+var accntPassword = "1234"
+accountCity = "Agra"
+
+//accountId = 2  NOT Allowed
+
+acctEmail = "hc@gm.com"
+accntPassword =567
+accountCity="Mau"
+
+console.log(accountId);
+console.table([accntPassword,accountId,acctEmail,accountCity]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; //Treat all JS code as newer version
+
+
+console.log(3 + 2)
+console.log("Nishant")
+
+let name = "Nishant"
+let age = 18
+let isLoggedIn = false
+
+
+// number =>  2 to the power 52
+// bigInt for larger numbers
+// string => ""
+// boolean => true/false
+// null => Standalone value
+// undefined => value not assigned
+// symbol => unique
+
+
+//OBJECT
+
+console.log(typeof undefined); // Undefined
+console.log(typeof null);      //Object
+
+
+
+
+
+
