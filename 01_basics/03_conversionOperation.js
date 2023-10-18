5fefb210f9e79bf585fc32ff554621dcaeced246
Nishant Singh <143175203+nishfit@users.noreply.github.com>
2023-10-18 19:25:42 +0000

    Conversions Done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,8 @@
-let score = 33
-
+let score = "sdsad"
+let namu = "Kisshh"
+let isBoolean = 1;
 console.log(typeof score);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof namu);
+console.log(typeof isBoolean);
---
+++ 01_basics/03a_conversions.js
@@ -0,0 +1,15 @@
+let val =3;
+let negValue =-val;
+
+console.log(negValue);
+console.log(typeof negValue);
+
+let str1 = "Hello "
+let str2 = "Nishant"
+console.log(str1+str2);
+
+console.log("1"+1);
+
+console.log("1"+2+3);
+
+console.log(1+2+"3");
