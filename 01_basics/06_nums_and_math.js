7eed5f46296388078bcf98c727bea4ea3c45d3c1
Nishant Singh <143175203+nishfit@users.noreply.github.com>
2023-10-20 20:36:45 +0000

    06 num and math

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -2,4 +2,30 @@ const score = 400;
 console.log(score);
 
 const balance = new Number(100);
-console.log(balance);
+console.log(balance); 
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+console.log(`************************`);
+const newBalance = new Number(105.126);
+console.log(newBalance.toFixed(2));
+console.log(newBalance.toPrecision(2));
+console.log(newBalance.toPrecision(3));
+console.log(newBalance.toPrecision(4));
+console.log(`******************`);
+
+
+//covert to indian currency system
+const hundreds = 1000000
+console.log(hundreds.toLocaleString(`en-IN`));
+
+/* *************** MATHS*********/
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(10.5));
+console.log(Math.min(10,4,8,9,22));
+console.log(`******`);
+console.log(Math.random());
+const gumgum=Math.random()*100;
+console.log(gumgum.toFixed(0));
