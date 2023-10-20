672177ac5bf1b73e749885c67a7327cc529adf42
Nishant Singh <143175203+nishfit@users.noreply.github.com>
2023-10-20 19:13:53 +0000

    goodnight for the day

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,27 @@
+const name = "Nishant ";
+const repoCount =50;
+// console.log(name + repoCount + " Singh");
+//OUTDATED
+
+//****Use***//
+//``````back ticks`````//
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+/*Initializing a String */
+const gameName = new String(`Counter Strike`);
+
+console.log(gameName);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.indexOf('t'));
+
+const subString = gameName.substring(0,4);
+console.log(subString);
+
+const sliceString = gameName.slice(-4,0);
+console.log(sliceString+': Slice string');
+
+const newString = '   Hello    guys i am here   ';
+console.log(newString);
+console.log(newString.trim());
